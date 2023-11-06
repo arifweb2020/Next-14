aac8179ec608632c25f1c71e8f376fb31a3b738e
arifweb2020 <arifhussainb2019@gmail.com>
2023-11-06 15:48:49 +0530

    use router

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-//"use-client";
+"use client";
 import Link from 'next/link';
 import styles from './page.module.css';
-//import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  // const navigate = useRouter();
+  const navigate = useRouter();
 
-  // const pageNavigate = (params: string) => {
-  //   navigate.push(params);
-  // }
+  const pageNavigate = (params: string) => {
+    navigate.push(params);
+  }
 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <h1>Test</h1>
         <Link href='/about'>Go to about page</Link>
-        {/* <button onClick={() => pageNavigate('/services')}>Services</button> */}
+        <button onClick={() => pageNavigate('/services')}>Services</button>
       </div>
     </main>
   );
