64045ecb4f78165f3fc2e2c10df57955ff271061
arifweb2020 <arifhussainb2019@gmail.com>
2023-11-07 13:28:16 +0530

    loading feature

---
+++ app/product/loading.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+const Loading = () => {
+  return (
+    <div className=''>
+      <div className="loading">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+    </div>
+  )
+}
+
+export default Loading
