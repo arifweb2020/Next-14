232aa08ad9108276998e9cd96539c2a16a194481
arifweb2020 <arifhussainb2019@gmail.com>
2023-11-07 14:04:59 +0530

    fix about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 
-export const About = () => {
+const About = () => {
   return (
     <div>About<br/>
     <Link href="/about/company-profile">Company profile</Link><br/><br/>
